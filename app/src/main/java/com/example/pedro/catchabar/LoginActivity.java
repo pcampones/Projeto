d6f8016c7b00d33c495dceab3ac41d5b5ea87404
Pedro Campones <pedro.campones@hotmail.com>
2016-11-05 16:19:56 +0000

    Sedung

--- app/src/main/java/com/example/pedro/catchabar/LoginActivity.java
+++ app/src/main/java/com/example/pedro/catchabar/LoginActivity.java
@@ -1,15 +1,40 @@
 package com.example.pedro.catchabar;
 
+import android.content.Intent;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    private TextView wLogIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        wLogIn = (TextView) findViewById( R.id.textView);
+
+        wLogIn.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+
+            //    Intent intent = new Intent(this,MapsActivity.class);
+
+            //    startActivity(intent);
+
+
+            }
+
+
+        });
+
     }
+
 
 
 
